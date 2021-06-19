e0c37c4b86ad6bfb2e903c86c8aa87fad2e21756
WinstonMoh <chepson4@gmail.com>
2021-06-19 17:49:55 -0400

    User authentication done(sign up and sign in)

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # CryptoTracker - Track your cryptocurrency portfolio
 Crytocurrency tracker to aggregate portfolio from multiple wallets. Designed with flutter.
+
+## What I learned
+- How to integrate Flutter with Firebase for user authentication.
+    - FlutterFire: Authentication
+    - Cloud Firestore: Databases
+    - Asynchronous methods with await().
+    - Access data from Firebase.
---
+++ lib/ui/home_view.dart
@@ -0,0 +1,69 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:crypto_tracker/ui/add_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(color: Colors.white),
+        child: Center(
+          child: StreamBuilder(
+            stream: FirebaseFirestore.instance
+                .collection('Users')
+                .doc(FirebaseAuth.instance.currentUser!.uid)
+                .collection('Coins')
+                .snapshots(),
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (!snapshot.hasData) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+
+              return ListView(
+                children: snapshot.data!.docs.map((document) {
+                  return Container(
+                    child: Row(
+                      children: [
+                        Text("Coin: ${document.id}"),
+                        Text("Price: ${document.data()}")
+                      ],
+                    ),
+                  );
+                }).toList(),
+              );
+            },
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AddView(),
+            ),
+          );
+        },
+        backgroundColor: Colors.blue,
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
